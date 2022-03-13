e8464f02483a4f1744ac8290257e332aa121a3cb
Sahil Bhatia <88426479+SahilBht@users.noreply.github.com>
2022-03-13 13:26:14 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details){ return {cancel : true}},
-    { urls: ["*://*.zedo.com/*", "*://*.grammarly.com/*", "*://business.linkedin.com/*", "*://ads.google.com/googleads/*", "*://googleadservices.com/*", "*://try.bravesoftware.com/*", "*://shutterstock.com/ad/stock-footage/*", "*://wix.com/*"]},
+    { urls: ["*://*.zedo.com/*", "*://*.grammarly.com/*", "*://business.linkedin.com/*", "*://ads.google.com/googleads/*", "*://*.googleadservices.com/*", "*://try.bravesoftware.com/*", "*://shutterstock.com/ad/stock-footage/*", "*://wix.com/*"]},
     ["blocking"]
 )
